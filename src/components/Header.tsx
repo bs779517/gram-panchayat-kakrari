9383d475639af24d4c3b4cbd92a0421fdcea3b34
Ram Singh <bs779517@gmail.com>
2025-09-18 03:51:37 +0000

    logo ko thoda chhota karo tab lgao

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,12 @@ export function Header() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center h-20">
           <Link href="/" className="flex items-center gap-3 text-2xl font-bold font-headline text-primary">
-            <div className="relative w-16 h-16">
+            <div className="relative w-12 h-12">
               <Image 
                 src="https://storage.googleapis.com/project-spark-341200.appspot.com/images%2Fteams%2Fteam_1724068777011_86400%2FGENERATED_1724070004077.png" 
                 alt="ग्राम पंचायत ककरारी logo" 
-                width={64}
-                height={64}
+                width={48}
+                height={48}
                 className="object-contain"
               />
             </div>
